dd7841fc15a7eb746e6fcf57c91dd02fed60919b
Andres Crosa <andresmcrosa@gmail.com>
2022-12-04 17:51:03 -0300

    POST

--- app.js
+++ app.js
@@ -2,9 +2,20 @@ const express = require('express')
 
 const app = express()
 
+app.use(express.json())
+
 app.get('/',function(req, res){
 
     res.send('Hola mundo!')
+})
+
+app.post('/imagen', function (req, res){
+
+    const body = req.body
+
+    console.log(body)
+
+    res.send('Hola mundo desde el POST')
 })
 
 app.listen(3000)
